fb1318902cd8285452f3ea308ea511f02b996f76
ntrv <6658243+ntrv@users.noreply.github.com>
2017-11-05 18:44:52 +0900

    Fix spec_helper_ssh

--- spec/role/sample2/default_spec.rb
+++ spec/role/sample2/default_spec.rb
@@ -1,4 +1,4 @@
-require 'spec_helper'
+require 'spec_helper_ssh'
 
 describe package('apache2'), :if => os[:family] == 'ubuntu' do
   it { should be_installed }
--- spec/spec_helper_ssh.rb
+++ spec/spec_helper_ssh.rb
@@ -1,25 +1,14 @@
 require 'serverspec'
+require 'pathname'
 require 'net/ssh'
 
 set :backend, :ssh
+set :path, '/sbin:/usr/local/sbin:$PATH'
+set :request_pty, true
 
-RSpec.configure do |c|
-  c.before :all do
-    set :host, ENV['TARGET_HOST']
-    opts = Net::SSH::Config.for(c.host)
-    opts[:user] = ENV['SSH_USER'] || Etc.getlogin
-    opts[:password] = ENV['SSH_PASSWORD']
-    set :sudo_password, ENV['SUDO_PASSWORD']
-    set :ssh_options, opts
-  end
-end
-
-# Disable sudo
-# set :disable_sudo, true
-
-
-# Set environment variables
-# set :env, :LANG => 'C', :LC_MESSAGES => 'C'
-
-# Set PATH
-# set :path, '/sbin:/usr/local/sbin:$PATH'
+set :host, ENV['TARGET_HOST']
+opts = Net::SSH::Config.for(ENV['TARGET_HOST'])
+opts[:user] = ENV['SSH_USER'] || Etc.getlogin
+opts[:password] = ENV['SSH_PASSWORD']
+set :sudo_password, ENV['SUDO_PASSWORD']
+set :ssh_options, opts
