a14b55c8a9227432f48216e4519a40b450da3a60
ntrv <6658243+ntrv@users.noreply.github.com>
2017-11-08 03:56:27 +0900

    Create pip recipe and spec

--- recipes/pip/recipe.rb
+++ recipes/pip/recipe.rb
@@ -1,6 +1,15 @@
-require_relative '../../utils/utils'
+package 'epel-release' do
+  action :install
+  only_if node[:platform] == 'centos'
+  notifies :install, 'package[python-pip]', :immediately
+end
+
+package 'python-pip' do
+  action :nothing
+end
 
 execute 'pip::install' do
   command 'easy_install pip'
-  not_if Dotfiles::Utils.cmd_exists?('pip')
+  not_if 'type pip 1>/dev/null 2>&1'
+  only_if 'uname | grep -q Darwin'
 end
---
+++ spec/recipe/pip/default_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe yumrepo('epel'), :if => os[:family] == 'redhat' do
+  it { should exist }
+  it { should be_enabled }
+end
+
+describe command('type pip') do
+  its(:exit_status) { should eq 0 }
+end
