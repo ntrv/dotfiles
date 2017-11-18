6361ed8b0b487a741ad5ffe91ee875c5bacfdec0
ntrv <6658243+ntrv@users.noreply.github.com>
2017-11-18 16:59:13 +0900

    Fix description

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,7 +9,7 @@ when 'DOCKER'
   set :docker_image, ENV['DOCKER_IMAGE'] || 'centos:7'
   set :docker_container_create_options, {'Cmd' => ['/bin/sh']}
   Excon.defaults[:ssl_verify_peer] = false
-  puts 'This is via docker serverspec'
+  puts 'This is via docker'
 when 'SSH'
   set :backend, :ssh
   set :request_pty, true
@@ -19,5 +19,5 @@ when 'SSH'
   puts 'This is via SSH serverspec'
 else
   set :backend, :exec
-  puts 'This is via Local serverspec'
+  puts 'This is via Local'
 end
